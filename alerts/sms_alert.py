3cf404258f06647a2fab0e58d6fe2aae929fcc8c
DSKandikere <dishaaks@gmail.com>
2026-05-17 20:10:22 +0530

    Replace Twilio credentials with placeholders
    Updated Twilio credentials to placeholders for security.

--- alerts/sms_alert.py
+++ alerts/sms_alert.py
@@ -1,10 +1,10 @@
 from twilio.rest import Client
 
 # Replace with your Twilio credentials
-ACCOUNT_SID = "AC346e703cf0919e0cf229c8b494c643d3"
-AUTH_TOKEN = "16b00e1a6e147606acb2f527b6162ee9"
-TWILIO_NUMBER = "+1 727 800 3891"
-TARGET_NUMBER = "+917349347948"
+ACCOUNT_SID = "your_twilio_SID_number"
+AUTH_TOKEN = "your_twilio_AUTH_TOCKEN"
+TWILIO_NUMBER = "your_twilio_NUMBER"
+TARGET_NUMBER = "TARGET_NUMBER"
 
 def send_sms_alert(message):
     try:
