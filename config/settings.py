87c10154b0f5eafdd34f71ea8cf28ce1f5a14474
DSKandikere <dishaaks@gmail.com>
2026-05-17 20:17:57 +0530

    addinf missing files

---
+++ config/settings.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+MONGO_URI = os.getenv("MONGO_URI")
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
